6548c76bf56548c4870e4ca6387c6291250fb1d5
Dunnudilshad <dunnudilshad@gmail.com>
2025-07-17 15:59:37 +0530

    debug makes false

--- project_ats/settings.py
+++ project_ats/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1lu-6ya(=d+qgyy*q9f-v&%ckaj&wcdszr6-s7#_*q0oj^nhnu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'application-tracker-system.onrender.com']
 
